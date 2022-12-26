190c58ed655d17eccb3f7ed11bb3ebd5b53c8624
Dawitlior <dawitlior777@gmail.com>
2022-12-26 14:21:24 +0200

    add

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ import Post from "./models/Post.js";
 import {posts,users} from './data/index.js'
 
 
-/* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
@@ -27,11 +26,17 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"));
+app.use(morgan("common")); //"combined"//
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
@@ -44,15 +49,16 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-/** ROUTES WITH FILES */
+
 app.post("/auth/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
-/** ROUTES */
+
+
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-/* MONGOOSE SETUP */
+
 const PORT = process.env.PORT || 6001; 
 mongoose.set("strictQuery", false);
 
